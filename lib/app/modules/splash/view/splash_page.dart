1a7dd9a40e4639cf7fe5ac7d2582953f13a70ce3
Abner Silva <abner.computadores@outlook.com>
2020-10-01 19:29:57 -0300

    inicio projeto pizza_delivery_app

---
+++ lib/app/modules/splash/view/splash_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+
+  static const router = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:pizza_delivery_app/app/modules/splash/view/splash_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: SplashPage.router,
+    );
+  }
+}
